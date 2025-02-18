50dd2c2b3759c5778eb9042f1b38b8abc1e98080
didacfrndz <fernandezbarcodidac@fpllefia.com>
2025-02-18 17:00:13 +0100

    MenuConRouterHecho

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import VistaJuego from './vistas/VistaJuego';
 import VistaInstrucciones from './vistas/VistaInstrucciones';
 
@@ -7,6 +7,16 @@ function App() {
   return (
     <>
       <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Juego</Link>
+            </li>
+            <li>
+              <Link to="/about">Instrucciones</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/" element={<VistaJuego />} />
           <Route path="/about" element={<VistaInstrucciones />} />
